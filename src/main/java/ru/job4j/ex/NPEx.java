cd0acc0af369d0f6930914a6a4b0989e20748704
aleksandrbirih <genaset@mail.ru>
2025-08-08 15:33:57 +0700

    test

--- src/main/java/ru/job4j/ex/NPEx.java
+++ src/main/java/ru/job4j/ex/NPEx.java
@@ -3,6 +3,7 @@ package ru.job4j.ex;
 public class NPEx {
     public static void main(String[] args) {
         String[] shops = new String[5];
+
         shops[0] = "Petr Arsentev";
         for (int index = 0; index < shops.length; index++) {
             String element = shops[index];
